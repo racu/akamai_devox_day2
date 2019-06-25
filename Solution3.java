dcc9f78a34f4d024f05843d75fa5f16d1c60c2fb
radek czyz <radoslaw.czyz@gmail.com>
2019-06-25 23:20:02 +0200

    fix fibbo

--- Solution3.java
+++ Solution3.java
@@ -5,9 +5,9 @@ public class Solution3
         String text = args[0];
         int n = Integer.parseInt(text);
         long[] dyn = new long[n+1];
-        dyn[0]=1;
         dyn[1]=1;
-        for(int i=2; i<=n; i++)
+        dyn[2]=1;
+        for(int i=3; i<=n; i++)
             dyn[i] = dyn[i-1] + dyn[i-2];
         System.out.println(dyn[n]);
     }
