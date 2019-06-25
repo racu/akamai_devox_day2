698ddfa5963d6ac873a3de3b4b16f8ac5fda5025
radek czyz <radoslaw.czyz@gmail.com>
2019-06-25 23:54:41 +0200

    .

--- proj/src/Solution7.java
+++ proj/src/Solution7.java
@@ -3,7 +3,9 @@ public class Solution7
     public static void main(String[] args)
     {
         String text = args[0];
-        String[] items = text.split("((?<!/),)|(//)");
+        while(text.indexOf("//") != -1)
+            text = text.replaceAll("//","-");
+        String[] items = text.split("((?<!/),)");
         System.out.print(items.length);
     }
 }
