77c1999d4b650ac2970f759cd53dc8014f119109
radek czyz <radoslaw.czyz@gmail.com>
2019-06-25 23:56:11 +0200

    .

--- Solution7.java
+++ Solution7.java
@@ -3,8 +3,7 @@ public class Solution7
     public static void main(String[] args)
     {
         String text = args[0];
-        while(text.indexOf("//") != -1)
-            text = text.replaceAll("//","-");
+        text = text.replaceAll("//","-");
         String[] items = text.split("((?<!/),)");
         System.out.print(items.length);
     }
